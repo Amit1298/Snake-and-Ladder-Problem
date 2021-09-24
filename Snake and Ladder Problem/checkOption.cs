ed3fe629175bd8dd5bb16f624adf8814070ca8f9
Amit <amitkr24031995@gmail.com>
2021-09-24 18:32:41 +0530

    Adding Opition there

---
+++ Snake and Ladder Problem/checkOption.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace checkOption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int noPlay = 0;
+            int ladder = 1;
+            int snake = 2;
+            Random random = new Random();
+            int checkOpt = random.Next(0, 3);
+            if (checkOpt == noPlay)
+            {
+                Console.WriteLine("Player at Starting Position");
+            }
+            else if (checkOpt == ladder)
+            {
+                Console.WriteLine("Player reach to ladder");
+            }
+            else
+            {
+                Console.WriteLine("Player cut by Snake");
+            }
+        }
+    }
+}

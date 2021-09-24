ef6090a89d1490aff9bae8ea8d3a229304580605
Amit <amitkr24031995@gmail.com>
2021-09-24 17:51:35 +0530

    Adding the Player at Starting position

---
+++ Snake and Ladder Problem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Snake_and_Ladder_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int noStart = 0;
+
+            Random random = new Random();
+            int dice = random.Next(0,1);
+            if (dice == noStart)
+            {
+                Console.WriteLine("Player at Starting Position");
+            }
+            else
+            {
+                Console.WriteLine("Player move forword");
+            }
+        }
+    }
+}

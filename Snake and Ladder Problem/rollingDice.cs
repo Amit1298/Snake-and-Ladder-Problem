f711c2b4045663cc7b549a75342ec41e8675a6bc
Amit <amitkr24031995@gmail.com>
2021-09-24 18:21:58 +0530

    Adding Rolling Dice

---
+++ Snake and Ladder Problem/rollingDice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace rollingDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dice;
+            Random random = new Random();
+            dice = random.Next(1, 7);
+            Console.WriteLine("No. of shown in Dice is : " + dice);
+        }
+    }
+}

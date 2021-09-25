8879baaf9e347b768939168bf7a3e5e012281795
Amit <amitkr24031995@gmail.com>
2021-09-25 10:55:33 +0530

    Adding Start Position

---
+++ Snake and Ladder Problem/repeatPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace repeatStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 0;
+            int final = 100;
+            int n;
+            Random random = new Random();
+            n = random.Next(95, 101);
+            if (n == final)
+            {
+                Console.WriteLine("Starts from Starting Position");
+            }
+            else
+            {
+                Console.WriteLine("Play Continously");
+            }
+        }
+    }
+}
